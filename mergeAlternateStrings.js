be899d40a085739ebead294b0080470ae61bcba6
elanore <elanorejoseph@gmail.com>
2024-12-20 19:22:31 +0530

    alternate way using for loop

--- mergeAlternateStrings.js
+++ mergeAlternateStrings.js
@@ -20,3 +20,26 @@ var mergeAlternately = function (word1, word2) {
 
 // Example usage
 console.log(mergeAlternately("12345", "shiny"));
+
+//alternate ways
+//using for loop
+var mergeAlternately1 = function (word1, word2) {
+  let merged1 = "";
+  let x = 0,
+    y = 0;
+
+  // Add characters from both strings alternately
+  while (x < word1.length || y < word2.length) {
+    if (x < word1.length) {
+      merged1 += word1[x];
+      x++;
+    }
+    if (y < word2.length) {
+      merged1 += word2[y];
+      y++;
+    }
+  }
+
+  return merged1;
+};
+console.log(mergeAlternately1("12345", "bobby"));
