dcdb76bdbc8b7b537c32126ab4048afaed67e2d8
elanore <elanorejoseph@gmail.com>
2025-01-10 16:08:36 +0530

    is subsequence problem

---
+++ isSubsequence.js
@@ -0,0 +1,22 @@
+var isSubsequence = function (s, t) {
+  let i = 0,
+    j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+  }
+
+  return i === s.length;
+};
+
+// Example usage:
+let s1 = "abc",
+  t1 = "ahbgdc";
+console.log(isSubsequence(s1, t1)); // Output: true
+
+let s2 = "axc",
+  t2 = "ahbgdc";
+console.log(isSubsequence(s2, t2)); // Output: false
